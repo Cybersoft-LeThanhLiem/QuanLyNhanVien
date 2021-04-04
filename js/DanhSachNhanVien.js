8a6ed904de22c005c6ee9087789f26192839d0a4
Liem <leliem28@gmail.com>
2021-04-04 14:42:43 +0700

    Them tinh nang cap nhat Nhan Vien

--- js/DanhSachNhanVien.js
+++ js/DanhSachNhanVien.js
@@ -23,4 +23,11 @@ function DanhSachNhanVien() {
             this.mangNV.splice(viTri, 1);
         }
     }
+
+    this.capnhatNV = function (nv) {
+        var viTri = this.timNV(nv.tk);
+        if (viTri >= 0) {
+            this.mangNV[viTri] = nv;
+        }
+    }
 }
